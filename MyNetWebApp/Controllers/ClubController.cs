b12d18106b1b945b7d62e518b80c54b9286ab552
win <winlwinoocu@gmail.com>
2022-12-11 14:54:03 +0630

    add view for club and race, by getting data form db

---
+++ MyNetWebApp/Controllers/ClubController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetWebApp.Data;
+using MyNetWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyNetWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        // GET: /<controller>/
+        public IActionResult Index() // Controller
+        {
+            List<Club> clubs = _context.Clubs.ToList(); // get data from Model
+            return View(clubs); // show data in View
+        }
+    }
+}
+
---
+++ MyNetWebApp/Controllers/RaceController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyNetWebApp.Data;
+using MyNetWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyNetWebApp.Controllers
+{
+    public class RaceController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            List<Race> races = _context.Races.ToList();
+            return View(races);
+        }
+    }
+}
+
