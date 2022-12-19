f0e17d7b8e6dce211f0a72aea97150e5ff8ced5f
win <winlwinoocu@gmail.com>
2022-12-19 23:45:02 +0630

    add Register

--- MyNetWebApp/Controllers/AccountController.cs
+++ MyNetWebApp/Controllers/AccountController.cs
@@ -54,6 +54,42 @@ namespace MyNetWebApp.Controllers
             return View(loginViewModel);
 
         }
+
+		public IActionResult Register()
+		{
+			var response = new RegisterViewModel();
+			return View(response);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+		{
+			if (!ModelState.IsValid) return View(registerViewModel);
+
+			var user = await _userManager.FindByEmailAsync(registerViewModel.Email);
+			if (user != null)
+			{
+				TempData["Error"] = "This email address is already in use";
+                return View(registerViewModel);
+            }
+
+			var newUser = new AppUser
+			{
+				Email = registerViewModel.Email,
+				UserName = registerViewModel.Email,
+
+			};
+			var newUserResponse = await _userManager.CreateAsync(newUser, registerViewModel.Password);
+			if (newUserResponse.Succeeded) await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+            return RedirectToAction("Index", "Race");
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> Logout()
+		{
+			await _signInManager.SignOutAsync();
+			return RedirectToAction("Index","Race");
+		}
 	}
 }
 
---
+++ MyNetWebApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyNetWebApp.ViewModels
+{
+	public class RegisterViewModel
+	{
+		[Required(ErrorMessage = "Email address is required")]
+		[Display(Name ="Email address")]
+		[DataType(DataType.EmailAddress)]
+		public string Email { get; set; }
+
+		[Required(ErrorMessage = "Password is required!")]
+		[DataType(DataType.Password)]
+		public string Password { get; set; }
+
+        [Required(ErrorMessage = "Confirm password is required!")]
+        [Display(Name = "Confirm password")]
+		[DataType(DataType.Password)]
+		[Compare("Password", ErrorMessage = "Password do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
+
